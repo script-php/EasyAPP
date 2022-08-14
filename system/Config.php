d3c44c8cc588aaff17a1403d3708e84e24f84596
- ̗̀YoYo ̖́- <43702866+YoYoDeveloper@users.noreply.github.com>
2022-08-14 10:17:11 +0300

    support extra app folders (example: admin panels)

--- system/Config.php
+++ system/Config.php
@@ -16,35 +16,34 @@ class Config {
     public $url     = NULL;
     public $main_controller     = NULL;
     public $error_controller    = NULL;
+    public $session             = true;
 
     // Framework config
     public $query               = 'route';
     public $dir_app             = 'app/';
-    public $dir_controller      = 'app/controller/';
-    public $dir_model           = 'app/model/';
-    public $dir_view            = 'app/view/';
-    public $dir_language        = 'app/language/';
+    public $dir_controller      = 'controller/';
+    public $dir_model           = 'model/';
+    public $dir_view            = 'view/';
+    public $dir_language        = 'language/';
 
     // System
     public $dir_system          = 'system/';
-    public $dir_framework       = 'system/Framework/';
-    public $dir_library         = 'system/Library/';
+    public $dir_framework       = 'Framework/';
+    public $dir_library         = 'Library/';
     public $dir_storage         = 'storage/'; # system/Storage
 
     public $debug               = false;
 
     public function __construct(string $path = '') {
-
-        $this->main_controller      = $path . $this->main_controller;
-        $this->error_controller     = $path . $this->error_controller;
+        
         $this->dir_app              = $path . $this->dir_app;
-        $this->dir_controller       = $path . $this->dir_controller;
-        $this->dir_model            = $path . $this->dir_model;
-        $this->dir_view             = $path . $this->dir_view;
-        $this->dir_language         = $path . $this->dir_language;
+        $this->dir_controller       = $this->dir_app . $this->dir_controller;
+        $this->dir_model            = $this->dir_app . $this->dir_model;
+        $this->dir_view             = $this->dir_app . $this->dir_view;
+        $this->dir_language         = $this->dir_app . $this->dir_language;
         $this->dir_system           = $path . $this->dir_system;
-        $this->dir_framework        = $path . $this->dir_framework;
-        $this->dir_library          = $path . $this->dir_library;
+        $this->dir_framework        = $this->dir_system . $this->dir_framework;
+        $this->dir_library          = $this->dir_system . $this->dir_library;
         $this->dir_storage          = $path . $this->dir_storage;
         
     }
---
+++ system/Framework.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+* @package      EasyAPP Framework
+* @version      v1.2.6
+* @author       YoYo
+* @copyright    Copyright (c) 2022, script-php.ro
+* @link         https://script-php.ro
+*/
+
+require PATH . 'System/Config.php'; // framework config
+
+$config = new System\Config(PATH);
+
+if($config->session) {
+    session_start();
+}
+
+if(defined('DIR_APP')) {
+    $config->dir_app = DIR_APP;
+    $config->dir_controller = $config->dir_app . 'controller/';
+    $config->dir_model = $config->dir_app . 'model/';
+    $config->dir_language = $config->dir_app . 'language/';
+    $config->dir_view = $config->dir_app . 'view/';
+}
+
+if($config->debug) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
+
+include rtrim($config->dir_system, '\\/ ') . DIRECTORY_SEPARATOR . 'Autoloader.php';
+
+$loader = new System\Autoloader();
+
+$loader->load([
+    'namespace' => 'System\Framework',
+    'directory' => $config->dir_framework,
+    'recursive' => true
+]);
+
+$loader->load([
+    'namespace' => 'System\Library',
+    'directory' => $config->dir_library,
+    'recursive' => true
+]);
+
+include $config->dir_system . 'Controller.php';
+include $config->dir_system . 'Model.php';
+
+require $config->dir_app . 'config.php'; // app config
+require $config->dir_app . 'helper.php'; // custom functions
+
+pre($config);
+
+$registry = new System\Framework\Registry();
+
+$registry->set('config', $config);
+$registry->set('db', new System\Framework\DB($config->db_hostname,$config->db_database,$config->db_username,$config->db_password,$config->db_port)); // database connection
+$registry->set('hooks', new System\Framework\Hook($registry));
+$registry->set('util', new System\Framework\Util($registry));
+$registry->set('mail', new System\Framework\Mail($registry));
+$registry->set('load', new System\Framework\Load($registry));
+
+$language = new System\Framework\Language($registry);
+$language->directory('ro-ro');
+$registry->set('language', $language);
+$request = new System\Framework\Request($registry);
+$registry->set('request', $request);
+
+$router = new System\Framework\SimpleRouter($registry);
+$router->loadPage();
+$request->end_session();
