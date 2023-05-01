e1bfd9ad6692454c037ce41c7eb2103bb18c3e70
- ̗̀YoYo ̖́- <l.voinea@yahoo.com>
2023-05-01 08:44:12 +0300

    .

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'script-php/easyapp',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6bdf66adf2e6ec61991a10022d9772adbb9e0103',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'script-php/easyapp' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6bdf66adf2e6ec61991a10022d9772adbb9e0103',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
