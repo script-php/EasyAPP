012b221539b6b50040230e5a2a18b8186f5170ab
YoYo <l.voinea@yahoo.com>
2025-10-08 02:00:47 +0300

    update

--- app/router.php
+++ app/router.php
@@ -1,1 +1,22 @@
 <?php
+
+// patterns
+$router->pattern('id', '[0-9]+');
+$router->pattern('page', '[0-9]+');
+
+# GET
+$router->get('/', 'home');
+
+$router->get('/error', 'not_found');
+
+
+# POST
+
+# PUT
+
+# DELETE
+
+# PATCH
+
+# Fallback route
+$router->fallback('not_found'); 
--- config.php
+++ config.php
@@ -1,23 +1,34 @@
 <?php
 date_default_timezone_set('Europe/Bucharest');
 
-$config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'].'/tests/'; 
-$config['url'] = 'http://'.$_SERVER['SERVER_NAME'].'/tests/';
-    
-$config['db_hostname'] = ''; 
-$config['db_database'] = '';
-$config['db_username'] = '';
-$config['db_password'] = '';
+/**
+ * Load environment variables from .env file
+ * This framework uses a custom EnvReader class to load environment variables.
+ * Make sure to create a .env file in the root directory of your project.
+ * The .env file should contain key-value pairs in the format KEY=VALUE.
+ * Example:
+ *   DEBUG=true
+ *   ENVIRONMENT=dev
+ * We can use arrays by separating values with commas.
+ * Example:
+ *   SERVICES=service1,service2,service3
+ *   or
+ *   SERVICES=["service1","service2","service3"]
+ *   or
+ *   SERVICES={"service1","service2","service3"}
+ * 
+ * 
+ * This config can be overridden in app/config.php file.
+ * For having different settings for app and framework.
+ */
 
-$config['domain'] = $_SERVER['SERVER_NAME']; // used for login
-$config['session_name'] = 'adm-ck'; // used for login
-$config['session_time'] = 31556926;
 
-$config['servers_upload'] = [
-    'local' => [
-        'url' => 'http://'.$_SERVER['SERVER_NAME'].'/storage/uploads/',
-    ]
-];
+$_SERVER['SERVER_NAME'] = !empty(env('DOMAIN')) ? env('DOMAIN') : 'localhost'; // TODO: change this with your domain
 
-$config['debug'] = true;
+$config['base_url'] = !empty(env('BASE_URL')) ? env('BASE_URL') : 'http://'.$_SERVER['SERVER_NAME'].'/'; // base url of the app, with trailing slash
+$config['url'] = !empty(env('URL')) ? env('URL') : 'http://'.$_SERVER['SERVER_NAME'].'/'; // full url of the app, with trailing slash
 
+$config['domain'] = !empty(env('DOMAIN')) ? env('DOMAIN') : 'localhost'; // used for cookie domain
+$config['session_name'] = !empty(env('SESSION_NAME')) ? env('SESSION_NAME') : 'session'; // used for login
+$config['session_time'] = !empty(env('SESSION_TIME')) ? env('SESSION_TIME') : 31556926;
+$config['services'] = !empty(env('SERVICES')) ? env('SERVICES') : []; // list of services to load (already parsed as array)
